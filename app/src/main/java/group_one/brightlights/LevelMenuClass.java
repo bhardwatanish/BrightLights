cbcd1a2aee2cdab750ac53441f7774b0a31bdfb5
David Zhang <davidlizhang98@gmail.com>
2019-03-29 15:29:11 +0000

    completed the basic Make Level page

---
+++ app/src/main/java/group_one/brightlights/LevelMenuClass.java
@@ -0,0 +1,55 @@
+package group_one.brightlights;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LevelMenuClass extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level_menu);
+        Button l1,l2,l3,ml,multiBut;
+        l1=(Button) findViewById(R.id.l1);
+        l2=(Button) findViewById(R.id.l2);
+        l3=(Button) findViewById(R.id.l3);
+        ml=(Button) findViewById(R.id.ml);
+        multiBut = (Button) findViewById(R.id.multiBut);
+        l1.setOnClickListener(this);
+        l2.setOnClickListener(this);
+        l3.setOnClickListener(this);
+        ml.setOnClickListener(this);
+        multiBut.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.l1:
+                Intent i=new Intent(getApplicationContext(), group_one.brightlights.NineLightModeClass.class).putExtra("level",1);
+                startActivity(i);
+                break;
+            case R.id.l2:
+                Intent j=new Intent(getApplicationContext(), group_one.brightlights.NineLightModeClass.class).putExtra("level",2);
+                startActivity(j);
+                break;
+            case R.id.l3:
+                Intent k=new Intent(getApplicationContext(), group_one.brightlights.NineLightModeClass.class).putExtra("level",3);
+                startActivity(k);
+                break;
+            case R.id.ml:
+                Intent l=new Intent(this, MakeLevel.class);
+                startActivity(l);
+                break;
+            case R.id.multiBut:
+                Intent m=new Intent(getApplicationContext(), group_one.brightlights.NineLightModeClass.class).putExtra("level",0);
+                startActivity(m);
+                break;
+        }
+    }
+}
