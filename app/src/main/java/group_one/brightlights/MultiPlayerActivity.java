55962b19fd193b6da780c52cffbcc155404c9869
Aideen Fay <fayai@tcd.ie>
2019-04-06 11:02:06 +0100

    feat: Load levels from firebase

--- app/src/main/java/group_one/brightlights/MultiPlayerActivity.java
+++ app/src/main/java/group_one/brightlights/MultiPlayerActivity.java
@@ -1,12 +1,22 @@
 package group_one.brightlights;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import group_one.brightlights.GamesAdapter;
@@ -14,7 +24,8 @@ import group_one.brightlights.R;
 
 public class MultiPlayerActivity extends AppCompatActivity {
 
-    List<String> Games = Arrays.asList("16lights","k6986774","kfkgmm");
+    List<String> Games = new LinkedList<String>();
+    GamesAdapter adapter = new GamesAdapter(Games);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +34,51 @@ public class MultiPlayerActivity extends AppCompatActivity {
 
         RecyclerView rvGames = (RecyclerView) findViewById(R.id.rvGames);
 
-        GamesAdapter adapter = new GamesAdapter(Games);
-
         rvGames.setAdapter(adapter);
         rvGames.setLayoutManager(new LinearLayoutManager(this));
+
+        setupFirebase();
     }
 
+    private void setupFirebase()
+    {
+        FirebaseDatabase.getInstance().getReference("games").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String gameId = dataSnapshot.getKey();
+                if (!Games.contains(gameId))
+                    Games.add(gameId);
 
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String gameId = dataSnapshot.getKey();
+                if (!Games.contains(gameId))
+                    Games.add(gameId);
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                String gameId = dataSnapshot.getKey();
+                if (Games.contains(gameId))
+                    Games.remove(gameId);
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
