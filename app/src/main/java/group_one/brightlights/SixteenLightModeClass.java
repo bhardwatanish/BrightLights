93f671b1b839f820ec6a22a038173cb6f3fb03c3
shikhar <shikhar1chaudhary@gmail.com>
2019-04-08 17:52:56 +0100

    working on show level

---
+++ app/src/main/java/group_one/brightlights/SixteenLightModeClass.java
@@ -0,0 +1,299 @@
+package group_one.brightlights;
+
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import java.util.Random;
+
+
+public class SixteenLightModeClass extends AppCompatActivity implements View.OnClickListener
+{
+
+    private Button[][] buttons = new Button[4][4];
+    private TextView textViewPlayer1;
+    private int[][] color= new int[4][4];
+    private String gameId ="16lights";
+    private static final String TAG = "ColorCoding";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        FirebaseDatabase.getInstance().getReference().child("games")
+                .child(gameId)
+                .setValue(null);
+
+        FirebaseDatabase.getInstance().getReference().child("games")
+                .child(gameId)
+                .child("RESET")
+                .setValue(System.currentTimeMillis());
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.sixteen_light_mode);
+        textViewPlayer1 = findViewById(R.id.text1);
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+                setarray(i,j);
+                setboardcolor(i,j);
+            }
+        }
+        setlevel();
+        Button reset;
+        reset=(Button) findViewById(R.id.reset);
+        reset.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //if (((Button) v).getText().toString().equals("x")){
+        switch(v.getId()){
+            case R.id.button_00:
+                swap(0,0);
+                break;
+            case R.id.button_01:
+                swap(0,1);
+                break;
+            case R.id.button_02:
+                swap(0,2);
+                break;
+            case R.id.button_03:
+                swap(0,3);
+                break;
+            case R.id.button_10:
+                swap(1,0);
+                break;
+            case R.id.button_12:
+                swap(1,2);
+                break;
+            case R.id.button_11:
+                swap(1,1);
+                break;
+            case R.id.button_13:
+                swap(1,3);
+                break;
+            case R.id.button_20:
+                swap(2,0);
+                break;
+            case R.id.button_21:
+                swap(2,1);
+                break;
+            case R.id.button_22:
+                swap(2,2);
+                break;
+            case R.id.button_23:
+                swap(2,3);
+                break;
+            case R.id.button_31:
+                swap(3,1);
+                break;
+            case R.id.button_30:
+                swap(3,0);
+                break;
+            case R.id.button_32:
+                swap(3,2);
+                break;
+            case R.id.button_33:
+                swap(3,3);
+                break;
+            case R.id.reset:
+                resetarray();
+        }
+
+    }
+    private void setboardcolor(int i,int j){
+        // Random rand = new Random();
+        // color[i][j]= rand.nextInt(2) + 0;
+        if(color[i][j]==1) {
+            buttons[i][j].setBackgroundColor(Color.parseColor("red"));
+            buttons[i][j].setTextColor(Color.parseColor("red"));
+            buttons[i][j].setText("x");
+            FirebaseDatabase.getInstance().getReference().child("games")
+                    .child(gameId)
+                    .child(i + "_" + j)
+                    .setValue("1");
+
+        }
+        else{
+            buttons[i][j].setBackgroundColor(Color.parseColor("#a4c639"));
+            buttons[i][j].setTextColor(Color.parseColor("#a4c639"));
+            buttons[i][j].setText("0");
+            FirebaseDatabase.getInstance().getReference().child("games")
+                    .child(gameId)
+                    .child(i + "_" + j)
+                    .setValue("0");
+        }
+
+    }
+    private void setarray(int i,int j){
+//        Random rand = new Random();
+        //color[i][j]= rand.nextInt(2) + 0;
+        color[i][j]=1;
+        setboardcolor(i,j);
+    }
+    private  void resetarray(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                setarray(i,j);
+            }
+        }
+        setlevel();
+    }
+
+    private void swap(int i,int j){
+        if(i-1>=0){
+            if (color[i-1][j]==1){
+                color[i-1][j]=0;
+            }
+            else{
+                color[i-1][j]=1;
+            }
+        }
+        if(i+1<=3){
+            if (color[i+1][j]==1){
+                color[i+1][j]=0;
+            }
+            else{
+                color[i+1][j]=1;
+            }
+        }
+        if(j-1>=0){
+            if (color[i][j-1]==1){
+                color[i][j-1]=0;
+            }
+            else{
+                color[i][j-1]=1;
+            }
+        }
+        if(j+1<=3){
+            if (color[i][j+1]==1){
+                color[i][j+1]=0;
+            }
+            else{
+                color[i][j+1]=1;
+            }
+        }
+        if (color[i][j]==1){
+            color[i][j]=0;
+        }
+        else{
+            color[i][j]=1;
+        }
+
+        for (int k = 0; k < 4; k++) {
+            for (int l = 0; l < 4; l++) {
+
+                setboardcolor(k,l);
+            }
+        }
+
+    }
+    private void  do_move(int min_rounds){
+        if(checkforwin()){
+            min_rounds++;
+        }
+        else{
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    swap(i,j);
+
+                }
+            }
+        }
+    }
+    private boolean checkforwin(){
+        int flag;
+        flag=color[0][0];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(flag!=color[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private  void setlevel(){
+        int k= (int) getIntent().getIntExtra("level",0);
+        //int k=Integer.parseInt(s);
+        Random rand = new Random();
+        for(int l=0;l<k;l++){
+            int i= rand.nextInt(4) + 0;
+            int j= rand.nextInt(4) + 0;
+            swap(i,j);}
+
+    }
+
+
+
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+        Log.d(TAG, "setGameId: " + gameId);
+        FirebaseDatabase.getInstance().getReference().child("games")
+                .child(gameId)
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, String s) {
+                        if (dataSnapshot.getValue() == null) {
+                            return;
+                        }
+                        String key = dataSnapshot.getKey();
+                        if (!key.equals("RESET")) {
+                            int row = Integer.parseInt(key.substring(0, 1));
+                            int col = Integer.parseInt(key.substring(2, 3));
+                            Integer shape = dataSnapshot.getValue(Integer.class);
+
+
+                        }
+                    }
+
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, String s) {
+                        if (dataSnapshot.getValue() == null) {
+                            return;
+                        }
+                        if (dataSnapshot.getKey().equals("RESET")) {
+                            resetarray();
+
+                        }
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+    }
+
+}
